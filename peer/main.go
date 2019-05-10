7aa2e740c3a4becfd36278e522bb5473ac6ac53c
yufuyi <yufuyi@veredholdings.com>
2019-05-10 15:55:03 +0800

    add commit

--- peer/main.go
+++ peer/main.go
@@ -29,8 +29,10 @@ var mainCmd = &cobra.Command{
 func main() {
 
 	// For environment variables.
+	//使用viper获取配置文件信息以及运行服务器环境变量
 	viper.SetEnvPrefix(common.CmdRoot)
 	viper.AutomaticEnv()
+	//将其中下划线替换成".",以便匹配.yaml文件
 	replacer := strings.NewReplacer(".", "_")
 	viper.SetEnvKeyReplacer(replacer)
 
@@ -41,7 +43,7 @@ func main() {
 	mainFlags.String("logging-level", "", "Legacy logging level flag")
 	viper.BindPFlag("logging_level", mainFlags.Lookup("logging-level"))
 	mainFlags.MarkHidden("logging-level")
-
+	//设置执行peer version命令
 	mainCmd.AddCommand(version.Cmd())
 	mainCmd.AddCommand(node.Cmd())
 	mainCmd.AddCommand(chaincode.Cmd(nil))
